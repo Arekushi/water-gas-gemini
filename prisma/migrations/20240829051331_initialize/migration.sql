690db9cf0288664811316726711779e58791fcfa
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2024-08-29 02:31:21 -0300

    🗃️ Adjust MeasureType name to unique

---
+++ prisma/migrations/20240829051331_initialize/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `measurementDate` to the `measure` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_measure" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "customerId" TEXT NOT NULL,
+    "measureTypeId" TEXT NOT NULL,
+    "hasConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "imageUrl" TEXT NOT NULL,
+    "measurementDate" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "measure_measureTypeId_fkey" FOREIGN KEY ("measureTypeId") REFERENCES "measure_type" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "measure_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_measure" ("createdAt", "customerId", "hasConfirmed", "id", "imageUrl", "measureTypeId", "updatedAt") SELECT "createdAt", "customerId", "hasConfirmed", "id", "imageUrl", "measureTypeId", "updatedAt" FROM "measure";
+DROP TABLE "measure";
+ALTER TABLE "new_measure" RENAME TO "measure";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
